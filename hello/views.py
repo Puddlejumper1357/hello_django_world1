a3ac91f4dee3f99ef9aff90ed71eb94cdd14d2c9
Austin Brown <abrown1357@live.com>
2017-04-02 01:21:27 +0000

    added a time that you entered the page

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
+
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return HttpResponse("Hello, world "+"Today's Date and time is: "+ time.strftime("%c"))
+
